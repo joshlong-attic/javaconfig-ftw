6bcec1791318522d8e283b230f719f0983d8dae9
Josh Long <josh@joshlong.com>
2013-08-27 19:33:58 -0700

    renaming services.

--- services/src/main/java/javaconfigftw/data/mongodb/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package javaconfigftw.data.mongodb;
-
-import javaconfigftw.data.BaseCustomerRepository;
-
-public interface CustomerRepository extends BaseCustomerRepository, MongoCustomerRepository {
-}
